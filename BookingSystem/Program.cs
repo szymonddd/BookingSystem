b2945cfb93d24cfd92d2037e9ad4db14f7edac08
szymond <memememe@gmail.com>
2024-12-10 09:14:42 +0100

    klasa Concert

---
+++ BookingSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Concert
+{
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+    public int AvailableSeats { get; set; }
+
+    public Concert(string name, DateTime date, string location, int availableSeats)
+    {
+        Name = name;
+        Date = date;
+        Location = location;
+        AvailableSeats = availableSeats;
+    }
+}
